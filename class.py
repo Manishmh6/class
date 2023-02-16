d0dc51257b28d93f2bd18384e843a1ec091cc3c0
Manish Hingve <123451523+Manishmh6@users.noreply.github.com>
2023-02-16 13:55:09 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def cal():
+    a=int(input("Enter the value "))
+    b=int(input("Eeter the value "))
+    c=input("Enter the (+,-,*,/)")
+    if c=='+':
+        print("pluse",a+b)
+    elif c=='-':
+        print(a-b)
+    elif c=='*':
+        print(a+b)
+    elif c=='/':
+        print(a/b)
+cal()
+cal()
+cal()
+cal()
+
+
+# In[ ]:
+
+
+def Evan():
+    a = int(input("Enter the value = "))
+    if a%2==0:
+        print("Evan")
+    else:
+        print("Odd")
+Evan()
+
